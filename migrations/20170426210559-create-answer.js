493b419839da1d407477155eb1dddbc14069e052
Josh Gamble <josh.gamble33@gmail.com>
2017-04-26 14:24:35 -0700

    Create Answers model and set associations

---
+++ migrations/20170426210559-create-answer.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Answers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      QuestionId: {
+        type: Sequelize.INTEGER,
+        // the references property creates a foreign key constraint in the database.
+        references: {
+          // the model property takes a value that is the table name that
+          // QuestionId should refer to.
+          model: 'Questions',
+          // the key property refers to the column inside the Questions table
+          // that QuestionId points to.
+          key: 'id'
+        },
+        // setting onDelete property to cascade will make sure that all 
+        // Answers associated to a question are deleted when that question is deleted
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Answers');
+  }
+};
